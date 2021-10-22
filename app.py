c170ceadd28b4d2df44ea0f7121adfe798a832d1
Nitin malhotra <geekyoperand>
2021-10-23 02:19:20 +0530

    link changer

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from telethon import TelegramClient, events
 from telethon.tl import functions, types
 import requests
 from urllib.parse import unquote
+import json
 
 api_id = 6487373
 api_hash = '700f3b0513f9a1a321b28d4eed4a4140'
@@ -55,6 +56,39 @@ def removeOriginalSources(text):
     return text
 
 
+def changeLinkID(unshortenedUrl, textUrl, text):
+    updatedUrl = unshortenedUrl
+    headers = {
+        'accept': 'application/json, text/javascript, */*; q=0.01',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9',
+        'content-length': '454',
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'cookie': '_xsrf=5f7720bfcf79491fbe48d06c6ee45f5e; anon_u=cHN1X19lNDhjNTdjNi1hYzIxLTQ2MzAtYmFmZi05MTkzNjVkODQ0MWE=|1634892423|ee2d4502a4ec8d09e2766a372630b99b96ee8b2f; optimizelyEndUserId=oeu1634892421291r0.43377463188643617; _gcl_au=1.1.1069873274.1634892422; _mkto_trk=id:754-KBJ-733&token:_mch-bitly.com-1634892422140-61949; _ga=GA1.2.1729876833.1634892422; _gid=GA1.2.1321869772.1634892422; wow-modal-id-10=yes; anon_shortlinks=https://go.aws/3GdnfQD,https://amzn.to/2ZeoEp2; cookie_banner=1; _ga_567GCTL9BB=GS1.1.1634892421.1.1.1634892494.60',
+        'origin': 'https://bitly.com',
+        'referer': 'https://bitly.com/',
+        'sec-ch-ua': '"Google Chrome";v="95", "Chromium";v="95", ";Not A Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36',
+        'x-requested-with': 'XMLHttpRequest',
+        'x-xsrftoken': '5f7720bfcf79491fbe48d06c6ee45f5e'
+    }
+    payload = {
+        'url': unshortenedUrl
+    }
+
+    data = requests.post("https://bitly.com/data/anon_shorten",
+                         headers=headers, data=payload)
+    if data.status_code == 200:
+        updatedUrl = json.loads(data.text)['data']['link']
+        text = text.replace(textUrl, updatedUrl)
+    return text
+
+
 async def main():
     entity = await client.get_entity("teeeeeeesttttttttnmcfcnm")
     print("Started Listening to the messages")
@@ -87,6 +121,8 @@ async def main():
                         print("---Already Posted----", urlParts[0])
                         file.write("Already Posted: {}\n".format(urlParts[0]))
                     else:
+                        event.message.text = changeLinkID(
+                            url, lnk[0], event.message.text)
                         oldURLs.append(urlParts[0])
                         notPosted = notPosted + 1
                         if len(oldURLs) == 10000:
@@ -94,7 +130,7 @@ async def main():
                     print(len(oldURLs))
                     file.write("oldURLs: {}\n".format(oldURLs))
             if count > 0 and notPosted > 0:
-                event.message.text = removeOriginalSources(text)
+                event.message.text = removeOriginalSources(event.message.text)
                 await client.send_message(entity=entity, message=event.message)
                 file.write("<== Posted ==>\n")
             else:
